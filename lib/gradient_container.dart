94f68ea9529a6158337a4d19e1a74f2117f99670
Damilola Albert Odunaiya <daodunaiya@gmail.com>
2025-02-20 23:59:50 +0100

    ...

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/styled_text.dart';
 
-final startAlignment = Alignment.topLeft;
-final endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({
