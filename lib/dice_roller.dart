a70085802bd8fb99466cbe0e2801c6825517f8a0
Damilola Albert Odunaiya <daodunaiya@gmail.com>
2025-02-21 19:13:56 +0100

    dice roller added

---
+++ lib/dice_roller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/images/dice-3.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(activeDiceImage, width: 200),
+        SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(fontSize: 28),
+            // padding: EdgeInsets.only(top: 20),
+          ),
+          child: Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
